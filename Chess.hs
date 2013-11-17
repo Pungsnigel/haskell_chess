35728da6188ac71e5d9625862e44d04433fe6df8
Jesper Persson <jespert.persson@gmail.com>
2013-11-18 00:23:42 +0100

    Add functions for converting a piece to a char and back

--- Chess.hs
+++ Chess.hs
@@ -14,5 +14,31 @@ data Piece      = Piece  PieceColor PieceType
 -- Chess is played with only two colors
 data PieceColor = White | Black
 -- The different types of chess pieces. 
-data PieceType  = King | Queen | Bishop | Rock | Knight | Pawn
+data PieceType  = King | Queen | Bishop | Rook | Knight | Pawn
 
+showPiece :: Piece -> Char
+showPiece (Piece White King)   = 'K'
+showPiece (Piece White Queen)  = 'Q'
+showPiece (Piece White Bishop) = 'B'
+showPiece (Piece White Rook)   = 'R'
+showPiece (Piece White Knight) = 'N'
+showPiece (Piece White Pawn)   = 'P'
+showPiece (Piece Black King)   = 'k'
+showPiece (Piece Black Queen)  = 'q'
+showPiece (Piece Black Bishop) = 'b'
+showPiece (Piece Black Rook)   = 'r'
+showPiece (Piece Black Knight) = 'n'
+
+readPiece :: Char -> Piece
+readPiece 'K' = (Piece White King)
+readPiece 'Q' = (Piece White Queen)
+readPiece 'B' = (Piece White Bishop)
+readPiece 'R' = (Piece White Rook)
+readPiece 'N' = (Piece White Knight)
+readPiece 'P' = (Piece White Pawn)
+readPiece 'k' = (Piece Black King)
+readPiece 'q' = (Piece Black Queen)
+readPiece 'b' = (Piece Black Bishop)
+readPiece 'r' = (Piece Black Rook)
+readPiece 'n' = (Piece Black Knight)
+readPiece 'p' = (Piece Black Pawn)
